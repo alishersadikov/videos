38d64f8b2a6e42143f7939d4ee7e9a6fad8d21f7
Alisher Sadikov <alishersadikov@gmail.com>
2019-12-30 16:53:26 -0600

    First working youtube request

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,12 +1,10 @@
 import axios from "axios";
-import credentials from "../../credentials";
 
 export default axios.create({
-  baseURL: "https://googleapis.com/youtube/v3",
-  params: {
-    part: "snippet",
-    type: "video",
-    maxResult: 5,
-    key: credentials.API_KEY
-  }
+  baseURL: "https://www.googleapis.com/youtube/v3"
+  // params: {
+  //   part: "snippet",
+  //   type: "video",
+  //   maxResult: 5,
+  // }
 });
--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import credentials from "../credentials";
+import youtube from "../apis/youtube";
 
 class App extends React.Component {
+  onTermSubmit = term => {
+    debugger;
+    youtube.get("/search", {
+      params: {
+        q: term,
+        part: "snippet",
+        type: "video",
+        maxResult: 5,
+        key: credentials.API_KEY
+      }
+    });
+  };
+
   render() {
     return (
       <div className="ui container">
-        <SearchBar />
+        <SearchBar onFormSubmit={this.onTermSubmit} />
       </div>
     );
   }
