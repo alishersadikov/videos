a78098f33db66883b5bb6ac568068ccc3fbc0a02
Alisher Sadikov <alishersadikov@gmail.com>
2019-12-30 17:09:01 -0600

    Build VideoList component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import VideoList from "./VideoList";
 import credentials from "../credentials";
 import youtube from "../apis/youtube";
 
@@ -23,8 +24,8 @@ class App extends React.Component {
   render() {
     return (
       <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit} />I have{" "}
-        {this.state.videos.length} videos.
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
