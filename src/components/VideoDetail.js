358b520a711496b436bb4be6624405d1fb550575
Alisher Sadikov <alishersadikov@gmail.com>
2019-12-30 18:15:49 -0600

    Enable basic iframe for selected video

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,8 +1,23 @@
 import React from "react";
 
 const VideoDetail = ({ video }) => {
-  const displayContent = video ? video.snippet.title : "Loading...";
-  return <div>{displayContent}</div>;
+  // const displayContent = video ? video.snippet.title : "Loading...";
+  if (!video) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <div className="ui embed">
+        <iframe src={`https://youtube.com/embed/${video.id.videoId}`} />
+      </div>
+
+      <div className="ui segment">
+        <h4 className="ui header">{video.snippet.title}</h4>
+        <p>{video.snippet.description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default VideoDetail;
